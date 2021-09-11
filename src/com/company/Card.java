c2f851e0ff6c4eedc123f1b841a3d6064bbf7f79
Tfauves <tfauvelle@gmail.com>
2021-09-11 10:32:17 -0400

    defined classes.

---
+++ src/com/company/Card.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Card {
+    private int faceValue;
+    private String suit;
+
+    public Card(int faceValue, String suit) {
+        this.faceValue = faceValue;
+        this.suit = suit;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
 	// write your code here
     }
+
+
 }
