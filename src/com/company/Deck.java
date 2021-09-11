0eea2a747a50ead9cf64b036464af3ecdb696ef2
Travis Fauvelle <tfauvelle@gmail.com>
2021-09-11 18:00:32 -0400

    added numeric faceValue array.

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Deck {
     public List<Card> cards = new ArrayList<>();
     public String[] faceValue = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+//    public int[] faceNumValue = {11, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     public String[] suit = {"♦", "♠", "♣", "♥"};
 
     public Deck() {
