3767415a7f021c48d7627bff4f7080709105afda
Travis Fauvelle <tfauvelle@gmail.com>
2021-09-11 10:59:30 -0400

    declared list of cards in deck class.

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -1,7 +1,12 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Deck {
     Card playingCard;
+    private List<Card> cards = new ArrayList<>();
+
 
 
 }
