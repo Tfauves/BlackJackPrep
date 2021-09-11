b2d115553d5a14af18ac8fd78642232ec15964b6
Travis Fauvelle <tfauvelle@gmail.com>
2021-09-11 17:45:26 -0400

    added suit shapes

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Deck {
     public List<Card> cards = new ArrayList<>();
     public String[] faceValue = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-    public String[] suit = {"Diamond", "Spade", "Club", "Heart"};
+    public String[] suit = {"♦", "♠", "♣", "♥"};
 
     public Deck() {
         for (String values : faceValue) {
