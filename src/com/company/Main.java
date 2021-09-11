3b10aa46b8c0a0501c76bf939077ff3c3db3d269
Tfauves <tfauvelle@gmail.com>
2021-09-10 21:38:19 -0400

    Initial commit.

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+//        Build an application with a card and Deck class.
+//        The end result should be 5 completely random cards with no chance of repetition.
+//        No UI is required for this.
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
