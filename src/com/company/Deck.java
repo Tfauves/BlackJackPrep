23ad3bbfea387538d9c8d7977d7792db97c2bd89
Tfauves <tfauvelle@gmail.com>
2021-09-12 20:02:00 -0400

    changed cards list to deck.

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Deck {
-    private List<Card> cards = new ArrayList<>();
+    private List<Card> deck = new ArrayList<>();
     private String[] faceValue = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 //    private int[] faceNumValue = {11, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     private String[] suit = {"♦", "♠", "♣", "♥"};
@@ -13,15 +13,15 @@ public class Deck {
     public Deck() {
         for (String values : faceValue) {
             for (String suits : suit) {
-                cards.add(new Card(values, suits));
+                deck.add(new Card(values, suits));
             }
         }
-        Collections.shuffle(cards);
+        Collections.shuffle(deck);
     }
 
     public void deal() {
         for (int i = 0; i < 5; i++) {
-            System.out.println(cards.get(i).getFaceValue()+ " " + cards.get(i).getSuit());
+            System.out.println(deck.get(i).getFaceValue()+ " " + deck.get(i).getSuit());
         }
     }
 
